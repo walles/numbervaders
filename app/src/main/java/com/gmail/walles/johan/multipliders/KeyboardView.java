8408f63e281750f184ddaa078948b76547f272d8
Johan Walles <johan.walles@gmail.com>
2018-11-06 10:55:20 +0100

    Scale keyboard dynamically and correctly

--- app/src/main/java/com/gmail/walles/johan/multipliders/KeyboardView.java
+++ app/src/main/java/com/gmail/walles/johan/multipliders/KeyboardView.java
@@ -3,9 +3,20 @@ package com.gmail.walles.johan.multipliders;
 import android.content.Context;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
+import android.util.DisplayMetrics;
 import android.view.View;
 
 public class KeyboardView extends View {
+    /**
+     * This is how high we want the keyboard.
+     */
+    private static final double HEIGHT_MILLIMETERS = 40;
+
+    /**
+     * We never give the keyboard more height than this.
+     */
+    private static final double MAX_HEIGHT_PERCENT = 30;
+
     /**
      * The actual initialization is done in {@link #KeyboardView(Context, AttributeSet, int)}.
      */
@@ -28,7 +39,17 @@ public class KeyboardView extends View {
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
         int width = MeasureSpec.getSize(widthMeasureSpec);
-        int height = 200;
+
+        DisplayMetrics displayMetrics = getContext().getResources().getDisplayMetrics();
+        double millimetersToPixelsFactor = displayMetrics.ydpi / 25.4;
+        int height = (int)(HEIGHT_MILLIMETERS * millimetersToPixelsFactor);
+
+        // Limit height to a percentage of the allowed height
+        int maxHeight = (int)((MAX_HEIGHT_PERCENT / 100.0) * MeasureSpec.getSize(heightMeasureSpec));
+        if (height > maxHeight) {
+            height = maxHeight;
+        }
+
         setMeasuredDimension(width, height);
     }
 }
