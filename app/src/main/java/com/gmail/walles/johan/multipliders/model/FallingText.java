c652b05b7c29b3fd47757e05d54f0d0b04a2c7f3
Johan Walles <johan.walles@gmail.com>
2018-11-06 17:07:59 +0100

    Randomize falling text x coordinate

--- app/src/main/java/com/gmail/walles/johan/multipliders/model/FallingText.java
+++ app/src/main/java/com/gmail/walles/johan/multipliders/model/FallingText.java
@@ -4,14 +4,18 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 
+import java.util.Random;
+
 public class FallingText implements GameObject {
+    private static final Random RANDOM = new Random();
+
     /**
      * How long will it take this text to fall to the bottom of the screen?
      */
     private static final double MS_TO_BOTTOM = 5000;
     private static final double PERCENT_PER_MS = 100.0 / MS_TO_BOTTOM;
 
-    private double x = 0;
+    private double x = -30.0 + 60 * RANDOM.nextDouble();
     private double y = 0;
     private String text = "Johan";
     private final Paint paint;
